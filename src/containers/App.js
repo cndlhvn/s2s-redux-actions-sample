be4cdd30ac3ff034c58f59d1139259b151e54ab5
candle <7157959+cndlhvn@users.noreply.github.com>
2017-11-17 00:03:51 +0900

    Create routers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 class App extends Component {
+  constructor(props){
+    super();
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+          {this.props.children}
+        </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state, ownProps) => { return {}}
+const mapDispatchToProps = dispatch => bindActionCreators({},  dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
