b7c30b92dcfefd5c4fa5b0e6129c0cb4673b87a4
candle <7157959+cndlhvn@users.noreply.github.com>
2017-11-17 13:16:18 +0900

    Update CoinsIndex container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
 
 import { Provider } from 'react-redux'
 import { Router, browserHistory } from 'react-router'
